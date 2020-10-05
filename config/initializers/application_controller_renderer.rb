fdbeed3ab7e4a39590e4a3011befa06a31b19ecf
meghkknhm <meg.hiko.r@gmail.com>
2020-10-05 12:31:45 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '78794c46e4d9d83f44f11c1c954e0e57af288fdd34ba4b888d244ade077580f16d5650a5c6d4fca4e0a5b086f31032819276c52ee915ee2b0717919df2709418'
